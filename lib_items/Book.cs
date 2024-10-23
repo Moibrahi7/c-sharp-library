fc33315c327436bb911336be6d410e0d6df89049
Moibrahi7 <moibrahi7@gmail.com>
2024-10-23 01:30:49 -0400

    Feat: Added Ids to book creation

--- lib_items/Book.cs
+++ lib_items/Book.cs
@@ -27,31 +27,63 @@ namespace library{
             }
         }
 
+        public Book(string title, string owner, bool isCheckedOut, string author, int pageNum, Guid id){
+            this.title = title;
+            this.owner = owner;
+            this.isCheckedOut = isCheckedOut;
+            authors.Add(author);
+            this.pageNum = pageNum;
+            this.id = id; 
+        }
         public Book(string title, string owner, bool isCheckedOut, string author, int pageNum){
             this.title = title;
             this.owner = owner;
             this.isCheckedOut = isCheckedOut;
-            this.authors.Add(author);
+            authors.Add(author);
             this.pageNum = pageNum;
+            id = Guid.NewGuid(); 
         }
-
+        public Book(string title, string owner, bool isCheckedOut, List<string> authors, int pageNum, Guid id){
+            this.title = title;
+            this.owner = owner;
+            this.isCheckedOut = isCheckedOut;
+            this.authors = authors;
+            this.pageNum = pageNum;
+            this.id = id; 
+        }
         public Book(string title, string owner, bool isCheckedOut, List<string> authors, int pageNum){
             this.title = title;
             this.owner = owner;
             this.isCheckedOut = isCheckedOut;
             this.authors = authors;
             this.pageNum = pageNum;
+            id = Guid.NewGuid(); 
         }
+        public Book(string title, List<string> authors, int pageNum, Guid id){
+            this.title = title;
+            this.authors = authors;
+            this.pageNum = pageNum;
+            this.id = id; 
 
+        }
         public Book(string title, List<string> authors, int pageNum){
             this.title = title;
             this.authors = authors;
             this.pageNum = pageNum;
+            id = Guid.NewGuid(); 
+
         }
-         public Book(string title, string author, int pageNum){
+         public Book(string title, string author, int pageNum, Guid id){
             this.title = title;
-            this.authors.Add(author);
+            authors.Add(author);
             this.pageNum = pageNum;
+            this.id = id; 
+        }
+        public Book(string title, string author, int pageNum){
+            this.title = title;
+            authors.Add(author);
+            this.pageNum = pageNum;
+            id = Guid.NewGuid(); 
         }
     }
 }
